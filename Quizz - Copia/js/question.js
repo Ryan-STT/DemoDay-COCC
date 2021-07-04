29ccf6ab7f7b0ed895bcdaea895730b6e67ab298
Thaina Lima <81167635+thainalmatos@users.noreply.github.com>
2021-07-04 18:00:38 -0300

    Add files via upload

---
+++ Quizz - Copia/js/question.js
@@ -0,0 +1,34 @@
+//QUESTÕES DO QUIZ
+let questions = [
+    {
+        numb: 1,
+        question: "O que o fazendeiro cuida no dia a dia?",
+        answer:"A) Cuida da sua plantação, alimenta suas galinhas e ovelhas.",
+        options:[
+            "A) Cuida da sua plantação, alimenta suas galinhas e ovelhas.",
+            "B) Alimenta suas cabras.",
+            "C) Passeia pela fazenda."
+        ]
+    },
+    {
+        numb: 2,
+        question: "O que o homem vira na noite de lua cheia?",
+        answer:"B) Se torna um lobisomen.",
+        options:[
+            "A) Se transforma em um vampiro.",
+            "B) Se torna um lobisomen.",
+            "C) Se torna uma aranha."
+        ]
+    },
+    {
+        numb: 3,
+        question: "O que o lobisomem faz durante a noite?",
+        answer:"C) Contempla o luar uivando.",
+        options:[
+            "A) Assiste televisão.",
+            "B) Passeia pela fazenda.",
+            "C) Contempla o luar uivando."
+        ]
+    },
+
+];
